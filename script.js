4bfb9b319655b007004901b6d4e3bf9a23fe84d9
asukhorukova1 <asukhorukova@inbox.ru>
2017-06-15 20:06:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function init() {
+
+    //     $("img").hover(function(){
+    // $("h1").hide("slow");
+    //     },function(){
+    // $("h1").show("slow");
+    //     });
+
+
+    function getName(object) {
+        var fullPath = object.src;
+        var filename = fullPath.replace(/^.*[\\\/]/, '');
+        // or, try this, 
+        // var filename = fullPath.split("/").pop();
+
+
+        var filename = fullPath.replace(/^.*[\\\/]/, '');
+
+        var stringname = filename;
+        var newstringname = stringname.replace(".jpg", "");
+        var newstringname = newstringname.replace(".png", "");
+        var newstringname = newstringname.replace(".gif", "");
+        var newstringname = newstringname.replace("%20", " ");
+        var newstringname = newstringname.replace("%20", " ");
+        var newstringname = newstringname.replace("%3E", ">");
+        var newstringname = newstringname.replace("%20", " ");
+        var newstringname = newstringname.replace("%20", " ");
+        var newstringname = newstringname.replace("%20", " ");
+        var newstringname = newstringname.replace("%20", " ");
+        var name = newstringname;
+        // document.getElementById("h1").value = filename;
+        console.log(name);
+        $(object).on({
+            mouseenter: function() {
+                $("h1").text(name);
+            },
+            mouseleave: function() {
+                $("h1").text("RUSSIAN FOR JAKOB");
+            }
+        });
+    };
+       $('img').each(function(){
+    getName(this);
+   })
+
+
+
+
+
+
+    $("img").on({
+        mouseenter: function() {
+            $(this).css("box-shadow","0px 0px 10px pink");
+            // getName(this);
+        },
+        mouseleave: function() {
+            $(this).css("box-shadow","0px 0px 0px pink");
+            // getName(this) === false;
+        }
+
+    });
+
+
+
+
+    //     $("img").hover(function(){
+    // $("h1").innerHTML("slow");
+    //     },function(){
+    // $("h1").show("slow");
+    //     });
+}
+window.onload = init();
